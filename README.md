59b1579e30f3077c522d7ae9701a03f6e4717828
Toru Kobayashi <koba0004@gmail.com>
2016-05-12 11:54:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CORS Sandbox
+
+## Resources
+
+* https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+* https://www.w3.org/TR/cors/
+* https://fetch.spec.whatwg.org/#cors-protocol
---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  return fs.createReadStream('./index.html').pipe(res);
+});
+app.listen(3000, () => console.log('app is listening on port 3000...'));
+
+const api = express();
+
+api.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+api.options('/', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Max-Age', 86400);
+  res.status(204);
+  res.end();
+})
+api.post('/', (req, res) => {
+  res.json({result: 'response from api(get)'});
+});
+api.post('/error', (req, res) => {
+  res.status(500);
+  res.json({result: 'error response from api(get)'});
+});
+api.listen(3001, () => console.log('api is listening on port 3001'));
